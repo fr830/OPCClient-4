b1a25bd7bb539a71abbed51be767fa2db40f8522
Jxner <renxj0207@126.com>
2017-12-13 10:09:51 +0800

    v1.0

--- launch.sh
+++ launch.sh
@@ -1,4 +1,11 @@
 # launch.sh 
-
-
 # For git pull project from GitHub and compile project in java
+
+# pull project from GitHub
+git pull origin master
+
+# compile project
+javac -Djava.ext.dirs=./lib   -encoding utf8  Client.java
+
+# run project
+java -Djava.ext.dirs=./lib  Client
